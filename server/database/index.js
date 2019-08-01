870bb962f618e070fa4cbf6ce8b7812b97f5d59c
Prashant Rathi <48441582+prashantrathi123@users.noreply.github.com>
2019-08-01 20:39:05 +0530

    dpth

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,4 @@
 let mongoose = require('mongoose');
-/*dbPath= 'mongodb+srv://prashantrathi321:9557046589@insta-eyryk.mongodb.net/vartalap';*/
 var User = require('../models/userschema');
 dbPath ='mongodb://localhost:27017/sm';
 mongoose.connect(dbPath, {useNewUrlParser: true, useFindAndModify: false});
